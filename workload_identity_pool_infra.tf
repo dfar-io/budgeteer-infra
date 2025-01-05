562dbb1488b03514f91649a793e07247b941d226
Dave Farinelli <d@dfar.io>
2025-01-05 13:16:03 +0000

    add both roles

--- workload_identity_pool_infra.tf
+++ workload_identity_pool_infra.tf
@@ -10,9 +10,14 @@ resource "google_storage_bucket_iam_member" "storage_access_infra" {
 }
 
 # look for a more granular pre-defined role for this
-resource "google_organization_iam_member" "billing_viewer_infra" {
+resource "google_organization_iam_member" "viewer_infra" {
   org_id  = local.org_id
   role    = "roles/viewer"
+  member  = local.infra_principal
+}
+resource "google_organization_iam_member" "billing_viewer_infra" {
+  org_id  = local.org_id
+  role    = "roles/billing.viewer"
   member  = local.infra_principal
 }
 
