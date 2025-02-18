8611cd36a057537745114b6f6420c0b105cd257f
Dave Farinelli <d@dfar.io>
2025-02-18 00:01:45 +0000

    Finalized

--- cloud_run.tf
+++ cloud_run.tf
@@ -8,10 +8,21 @@ resource "google_cloud_run_v2_service" "ui" {
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
+      ports {
+        container_port = 80
+      }
     }
   }
 
   depends_on = [google_project_service.cloud_run_admin]
+  lifecycle {
+    ignore_changes = [
+      client,
+      client_version,
+      template[0].labels,
+      template[0].containers[0].image
+    ]
+  }
 }
 
 resource "google_cloud_run_service_iam_member" "public_access" {
@@ -27,4 +38,8 @@ resource "google_cloud_run_service_iam_member" "public_access" {
 resource "google_project_service" "cloud_run_admin" {
   project = google_project.project.project_id
   service = "run.googleapis.com"
+}
+resource "google_project_service" "cloud_build" {
+  project = google_project.project.project_id
+  service = "cloudbuild.googleapis.com"
 }
