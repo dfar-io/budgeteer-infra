8a8f7bf4380318555c5cc1d39610f43eee912722
Dave Farinelli <d@dfar.io>
2025-05-18 15:14:04 -0400

    Upgrade Terraform provider to 1.12.0

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ provider "github" {
 }
 
 terraform {
-    required_version = "~> 1.11.0"
+    required_version = "~> 1.12.0"
     required_providers {
         google = {
             source  = "hashicorp/google"
