400297ac8e99f81fd333ca6f6c6574dd69f49d4e
Dave Farinelli <d@dfar.io>
2025-08-26 13:50:49 -0400

    Update hashicorp/google to 1.13.0

--- providers.tf
+++ providers.tf
@@ -14,7 +14,7 @@ provider "github" {
 }
 
 terraform {
-    required_version = "~> 1.12.0"
+    required_version = "~> 1.13.0"
     required_providers {
         google = {
             source  = "hashicorp/google"
